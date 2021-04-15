4e120ac36f69469e20ef51532f939b8b3ee20dca
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-15 09:54:34 -0400

    offline functionality w indexeddb

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,7 +5,7 @@ const request = indexedDB.open('budget-tracker', 1);
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
 
-    db.createObjectStore('new_pizza', { autoIncrement: true });
+    db.createObjectStore('new_funds', { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
